924944407d1ae5c6124a1e728ddc291517643eec
mirza-adnan <mirza.adnan2205@gmail.com>
2021-09-30 20:11:45 +0600

    next day is highlighted after 1 pm of the curr day

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
-const date = document.querySelector(".date");
+const dateDisplay = document.querySelector(".date");
 const offlineBtn = document.querySelector(".offline-btn");
 const onlineBtn = document.querySelector(".online-btn");
 const offlineGrid = document.querySelector(".offline-grid");
 const onlineGrid = document.querySelector(".online-grid");
 
-offlineBtn.addEventListener("click", displayOfflineGrid);
-onlineBtn.addEventListener("click", displayOnlineGrid);
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
 
-updateDate();
-setInterval(updateDate, 1000);
+function updateDate() {
+    const today = new Date();
+    const day = days[today.getDay()];
+    const month = months[today.getMonth()];
+    const date = String(today.getDate());
+    const year = String(today.getFullYear());
+    dateDisplay.textContent = `${day}, ${month} ${date}, ${year}`;
+
+    const hour = today.getHours();
+    let dayLower;
+    if (hour > 13 && today.getDay() === 4) {
+        dayLower = days[(today.getDay() + 2) % 7].toLowerCase();
+    } else if (hour > 13 || (hour < 13 && today.getDay() === 5)) {
+        dayLower = days[(today.getDay() + 1) % 7].toLowerCase();
+    } else if (hour < 13) {
+        dayLower = day.toLowerCase();
+    }
+
+    const allCells = document.querySelectorAll(".grid-cell");
+    allCells.forEach((cell) => {
+        if (cell.classList.contains(dayLower)) {
+            cell.classList.add("active");
+        } else {
+            cell.classList.remove("active");
+        }
+    });
+}
 
 function displayOfflineGrid() {
     onlineBtn.classList.remove("active");
@@ -24,47 +70,8 @@ function displayOnlineGrid() {
     onlineGrid.classList.add("active");
 }
 
-function updateDate() {
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-    const days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ];
+offlineBtn.addEventListener("click", displayOfflineGrid);
+onlineBtn.addEventListener("click", displayOnlineGrid);
 
-    const today = new Date();
-    const day = days[today.getDay()];
-    const month = months[today.getMonth()];
-    const currDate = String(today.getDate());
-    const year = String(today.getFullYear());
-
-    date.textContent = `${day}, ${month} ${currDate}, ${year}`;
-
-    const dayLower = day.toLowerCase();
-
-    const allCells = document.querySelectorAll(".grid-cell");
-    allCells.forEach((cell) => {
-        if (cell.classList.contains(dayLower)) {
-            cell.classList.add("active");
-        } else {
-            cell.classList.remove("active");
-        }
-    });
-}
+updateDate();
+setInterval(updateDate, 1000);
