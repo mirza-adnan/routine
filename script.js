6079a23adddfa382ed7b42174d7d408468dabd36
mirza-adnan <mirza.adnan2205@gmail.com>
2021-09-30 15:58:45 +0600

    added function to update date and highlight classes of current day every second

--- script.js
+++ script.js
@@ -4,49 +4,67 @@ const onlineBtn = document.querySelector(".online-btn");
 const offlineGrid = document.querySelector(".offline-grid");
 const onlineGrid = document.querySelector(".online-grid");
 
-offlineBtn.addEventListener("click", () => {
+offlineBtn.addEventListener("click", displayOfflineGrid);
+onlineBtn.addEventListener("click", displayOnlineGrid);
+
+updateDate();
+setInterval(updateDate, 1000);
+
+function displayOfflineGrid() {
     onlineBtn.classList.remove("active");
     onlineGrid.classList.remove("active");
     offlineBtn.classList.add("active");
     offlineGrid.classList.add("active");
-});
+}
 
-onlineBtn.addEventListener("click", () => {
+function displayOnlineGrid() {
     offlineBtn.classList.remove("active");
     offlineGrid.classList.remove("active");
     onlineBtn.classList.add("active");
     onlineGrid.classList.add("active");
-});
+}
 
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-];
-const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-];
+function updateDate() {
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
 
-const today = new Date();
+    const today = new Date();
+    const day = days[today.getDay()];
+    const month = months[today.getMonth()];
+    const currDate = String(today.getDate());
+    const year = String(today.getFullYear());
 
-const day = days[today.getDay()];
-const month = months[today.getMonth()];
-const currDate = String(today.getDate());
-const year = String(today.getFullYear());
+    date.textContent = `${day}, ${month} ${currDate}, ${year}`;
 
-date.textContent = `${day}, ${month} ${currDate}, ${year}`;
+    const dayLower = day.toLowerCase();
+
+    const allCells = document.querySelectorAll(".grid-cell");
+    allCells.forEach((cell) => {
+        if (cell.classList.contains(dayLower)) {
+            cell.classList.add("active");
+        } else {
+            cell.classList.remove("active");
+        }
+    });
+}
